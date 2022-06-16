b30b9fb2218d3aac8b98ee20fcc5ff2901f3ccae
Phuong Tong <71061461+phuong-tong@users.noreply.github.com>
2022-06-15 21:07:08 -0700

    home

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./redux/reducer/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import reducerURL from "../../component/ListProduct/Module/Reducers/reducers";
+
+const rootReducer = combineReducers({
+  reducerURL,
+});
+export default rootReducer;
